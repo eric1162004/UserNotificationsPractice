93802a9554a8c8f297a268ca216b9a1dabb4634d
Eric Cheung <eric1162004@hotmail.com>
2022-03-11 11:53:25 -0800

    Initial Commit

---
+++ UserNotificationsPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserNotificationsPractice
+//
+//  Created by eric on 2022-03-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UserNotificationsPractice/UserNotificationsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserNotificationsPracticeApp.swift
+//  UserNotificationsPractice
+//
+//  Created by eric on 2022-03-11.
+//
+
+import SwiftUI
+
+@main
+struct UserNotificationsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
